a9dfa9f6cebdb2079505a0208a72036379ff5278
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-01-11 10:41:18 +0700

    update 2026-01-11

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,20 @@
 sudo apt update
 sudo apt install --no-install-recommends --no-install-suggests -y nodejs npm python3
 
-sudo cat >/etc/systemd/system/kiosk.service <<EOF
+sudo systemctl stop kiosk || true
+cat >~/kiosk.service <<EOF
 [Unit]
 Before=snapd.service
 
 [Service]
-WorkingDirectory=/home/s7usr/pbl5
-ExecStart=/bin/bash /home/s7usr/pbl5/scripts/run.sh
+WorkingDirectory=/home/$USER/pbl5
+ExecStart=/bin/bash /home/$USER/pbl5/scripts/run.sh
 
 [Install]
 WantedBy=basic.target
 EOF
+
+mv ~/kiosk.service /etc/systemd/system/kiosk.service
 
 sudo systemctl enable kiosk
 sudo systemctl start kiosk
