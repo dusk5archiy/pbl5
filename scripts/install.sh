d85aeed885ec0cda3c79ff00fa78b0b77edf4b2d
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-01-11 11:19:36 +0700

    add autologin scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,13 @@
 sudo apt update
 sudo apt install --no-install-recommends --no-install-suggests -y nodejs npm python3
 
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+sudo touch /etc/systemd/system/getty@tty1.service.d/override.conf
+sudo tee /etc/systemd/system/getty@tty1.service.d/override.conf >/dev/null <<EOF
+[Service]
+ExecStart=-/sbin/agetty --autologin $USER --noclear tty1
+EOF
+
 sudo systemctl stop kiosk || true
 sudo tee /etc/systemd/system/kiosk.service >/dev/null <<EOF
 [Unit]
@@ -16,6 +23,14 @@ EOF
 
 sudo systemctl enable kiosk
 sudo systemctl start kiosk
+
+sudo systemctl enable snapd
+sudo systemctl start snapd
+sudo snap install ubuntu-frame
+sudo snap set ubuntu-frame daemon=true
+sudo snap install chromium
+sudo snap set chromium url=http://localhost:3000
+snap connect chromium:wayland
 
 cd front
 if [[ ! -d "node_modules" ]]; then
