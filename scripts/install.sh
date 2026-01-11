5fe2e9c1df3745e86f2a166948583c1b91ae2148
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-01-11 10:34:31 +0700

    add systemd configuration

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,21 @@
 sudo apt update
 sudo apt install --no-install-recommends --no-install-suggests -y install nodejs npm python3
 
+sudo cat >/etc/systemd/system/kiosk.service <<EOF
+[Unit]
+Before=snapd.service
+
+[Service]
+WorkingDirectory=/home/s7usr/pbl5
+ExecStart=/bin/bash /home/s7usr/pbl5/scripts/run.sh
+
+[Install]
+WantedBy=basic.target
+EOF
+
+sudo systemctl enable kiosk
+sudo systemctl start kiosk
+
 cd front
 if [[ ! -d "node_modules" ]]; then
   npm install
