02dd54582a3ca18260f536538665c1efe2d3ca59
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-01-13 11:12:23 +0700

    add arch linux (not finished yet)

--- scripts/arch-install.sh
+++ scripts/arch-install.sh
@@ -1,13 +1,9 @@
+# NOT FINISHED YET !
+
 sudo pacman -S --needed --noconfirm \
   nodejs npm \
   python \
-  python-pyxdg \
-  vim \
-  which \
-  firefox \
-  xorg-server \
-  xorg-xinit \
-  openbox
+  firefox
 
 sudo tee /etc/systemd/system/kiosk-pbl5.service >/dev/null <<EOF
 [Unit]
@@ -22,22 +18,6 @@ WantedBy=basic.target
 EOF
 
 sudo systemctl enable --now kiosk-pbl5
-
-sudo tee /etc/systemd/system/kiosk-openbox.service >/dev/null <<EOF
-[Unit]
-Description=kiosk-openbox
-
-[Service]
-User=$USER
-Environment="DISPLAY=:0"
-Environment="XAUTHORITY=/home/$USER/.Xauthority"
-ExecStart=/usr/bin/startx /usr/bin/firefox --kiosk http://localhost:3000 -- :0 vt1
-
-[Install]
-WantedBy=basic.target
-EOF
-
-sudo systemctl enable --now kiosk-openbox
 
 cd front
 if [[ ! -d "node_modules" ]]; then
