978b1d66b9bb21d4aafc49061fa898965c4f6284
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-02-07 21:23:53 +0700

    decrease font in budget panel and buy modal

--- front/app/ui/game-board/BuyModal.tsx
+++ front/app/ui/game-board/BuyModal.tsx
@@ -17,7 +17,7 @@ export function BuyModal(props: BuyModalProps) {
   const buttonClassName = "p-[7cqw] rounded disabled:text-white disabled:bg-gray-300 border-2 border-white";
   return (
     <div className="w-full h-full flex flex-col gap-[1.5vw] justify-center">
-      <div className="w-full flex justify-center font-bold whitespace-nowrap text-[5cqw]">Bạn có muốn mua BĐS {bds} với giá {formatBudget(price)}?</div>
+      <div className="w-full flex justify-center font-bold whitespace-nowrap text-[4cqw]">Bạn có muốn mua BĐS {bds} với giá {formatBudget(price)}?</div>
       <div className="w-full flex justify-evenly px-[1.5vw] gap-[1.5vw] text-[6cqw]">
         <button className={buttonClassName + " bg-blue-300"} disabled={!can_buy} onClick={() => buyFunc({ response: 1 })}>Mua</button>
         <button className={buttonClassName + " bg-yellow-300"} onClick={() => buyFunc({ response: 0 })}>Đấu giá</button>
