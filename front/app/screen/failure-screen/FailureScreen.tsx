c7ca811fa496a3e56859c563c2d9a581a49733d6
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-02-07 23:10:46 +0700

    stringify using json

--- front/app/screen/failure-screen/FailureScreen.tsx
+++ front/app/screen/failure-screen/FailureScreen.tsx
@@ -7,7 +7,7 @@ export default function FailureScreen(error?: any) {
       {
         error != null &&
         <div className="w-full text-center text-[5vh] mt-[5vh] mb-[5vh]">
-          {String(error)}
+          {JSON.stringify(error)}
         </div>
       }
     </div>
