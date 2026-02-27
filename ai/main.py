fcbc0d91ebfb11b5872b0f8a831d8af0daf01c64
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-02-27 18:44:55 +0700

    fix allow methods

--- ai/main.py
+++ ai/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=False,
-    allow_methoaids=["*"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
